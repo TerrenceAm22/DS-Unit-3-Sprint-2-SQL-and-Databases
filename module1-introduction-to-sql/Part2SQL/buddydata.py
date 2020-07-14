a0cc5eba50fb342edcb4a37945ffebbe551bff3b
Terrence  Malone <terrenceam22@Terrences-MBP.hsd1.ga.comcast.net>
2020-07-14 12:04:04 -0400

    changed file structure

--- module1-introduction-to-sql/Part2SQL/buddydata.py
+++ module1-introduction-to-sql/Part2SQL/buddydata.py
@@ -2,8 +2,10 @@ import pandas
 from pandas import read_csv, DataFrame
 import sqlite3
 import os
-from os import path
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo=False)
+conn = sqlite3.connect('buddy.db')
 
-conn = sqlite3.connect('buddymove_holidayiq.csv')
-
-curs = conn.cursor()
+df = pandas.read_csv("buddymove_holidayiq.csv")
+df.to_sql('users', con=conn)
+engine.execute("SELECT * FROM users").fetchall()
