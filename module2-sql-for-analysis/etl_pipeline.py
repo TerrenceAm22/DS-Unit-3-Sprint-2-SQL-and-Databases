fd3b58dc28389c64857c0bf37f3d9386501fa4cc
Terrence  Malone <terrenceam22@Terrences-MBP.hsd1.ga.comcast.net>
2020-07-15 09:45:44 -0400

    new methods

--- module2-sql-for-analysis/etl_pipeline.py
+++ module2-sql-for-analysis/etl_pipeline.py
@@ -1,11 +1,18 @@
 import psycopg2
+from psycopg2.extras import execute_values
+import os
 import csv
-
+import pandas 
+from pandas import read_csv
 
 DB_NAME = 'ggrrjklv'
 DB_USER = 'ggrrjklv'
 DB_PASSWORD = 'cTA7aHWgPxf4kzOOBeKS5dhRUSW4sk5i'
 DB_HOST = 'ruby.db.elephantsql.com'
+
+
+
+
 
 ### Connecting to ELephantSQL
 class DatabaseConnection:
@@ -17,12 +24,38 @@ class DatabaseConnection:
         except:
             print("Cannot Connect to database")
     
+    def table_creation(self):
+        table_creation_query = """
+        DROP TABLE passengers;
+        CREATE TABLE IF NOT EXISTS passengers (
+            id SERIAL PRIMARY KEY,
+            survived integer,
+            pclass integer,
+            name varchar NOT NULL,
+            gender varchar NOT NULL,
+            age float,
+            sib_spouse_count integer,
+            parent_child_count integer,
+            fare float);
+            """
+        self.cursor.execute(table_creation_query)
+        
+        
+    
     def create_table(self):
         titanic = """
-                    CREATE TABLE titanic(
-                    id SERIAL PRIMARY KEY,
-                    name varchar(40) NOT NULL);
-                    """
+        CREATE TABLE IF NOT EXISTS passengers (
+            id SERIAL PRIMARY KEY,
+            survived bool,
+            pclass int,
+            name varchar,
+            sex varchar,
+            age int,
+            sib_spouse_count int,
+            parent_child_count int,
+            fare float8
+            );
+            """
         self.cursor.execute(titanic)
     
     def data(self):
@@ -30,11 +63,15 @@ class DatabaseConnection:
         self.cursor.copy_from(f, 'titanic', sep=',')
         f.close()
         self.connection.commit()
+    
+    def insert_data(self):
+        list_of_tuples = list(df.to_records(index=True))
+        insertion_query = "INSERT INTO passengers (id, survived, pclass, name, gender, age, sib_spouse_count, parent_child_count, fare) VALUES %s"
+        execute_values(cursor, insertion_query, list_of_tuples)
+        self.connection.commit()
         
-        
-
-
 if __name__=='__main__':
     database_connection = DatabaseConnection()
     #database_connection.create_table()
     database_connection.data()
+    database_connection.insert_data()
