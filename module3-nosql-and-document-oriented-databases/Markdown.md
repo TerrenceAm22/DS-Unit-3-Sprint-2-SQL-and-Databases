7965008f56f68a39bfe7943c8e85d1638680c9e1
Terrence  Malone <terrenceam22@Terrences-MBP.hsd1.ga.comcast.net>
2020-07-15 18:47:18 -0400

    assignment done I think

---
+++ module3-nosql-and-document-oriented-databases/Markdown.md
@@ -0,0 +1,5 @@
+"How was working with MongoDB different from working with PostgreSQL? What was easier, and what was harder?"
+
+Working with mongodb was a lot easier than Postgresql.
+It was more streamlined and straightfoward atleast pertaining to the task at hand today.
+I also like the UI for the website very well structured.
--- module3-nosql-and-document-oriented-databases/mongoqueries.py
+++ module3-nosql-and-document-oriented-databases/mongoqueries.py
@@ -1,10 +1,25 @@
 import pymongo
-
-
-#B_URI = ("mongodb+srv://TerrenceM23:<password>@cluster0-2cfdw.mongodb.net/<dbname>?retryWrites=true&w=majority")
-
-
-
-client = pymongo.MongoClient("mongodb+srv://TerrenceM23:<password>@cluster0-2cfdw.mongodb.net/<dbname>?retryWrites=true&w=majority")
+from pymongo import MongoClient
+import sqlite3
+client = pymongo.MongoClient("mongodb+srv://TerrenceM23:Paradox1998!@cluster0.2cfdw.mongodb.net/<test>?retryWrites=true&w=majority")
 db = client.test
-db.test.insert_many
+rpg_character = (1, "King Bob", 10, 3, 0, 0, 0)
+#db.test.insert_one({'rpg_character': rpg_character})
+rpg_doc = {
+    'sql_key': rpg_character[0],
+    'name': rpg_character[1],
+    'hp': rpg_character[2],
+    'level': rpg_character[3]
+}
+#db.test.insert_one(rpg_doc)
+#db.test.find_one(rpg_doc)
+
+sl_conn = sqlite3.connect('rpg_db.sqlite3')
+sl_curs = sl_conn.cursor()
+get_characters = 'SELECT * FROM charactercreator_character;'
+get_characters = sl_curs.execute(get_characters).fetchall()
+print(get_characters)
+
+get_characters = ({'get_characters': get_characters})
+
+db.test.insert_one(get_characters)
